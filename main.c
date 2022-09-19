6ae726ff8ae83d53065c3add20fbf1624cdb0f80
iagoscm <iagomatosousa@gmail.com>
2022-09-19 11:53:37 -0300

    adiciona prints de verificação

--- main.c
+++ main.c
@@ -11,6 +11,7 @@ typedef struct Cientista{
 
 void instala(Cientista ** dado, int salario, int linha);
 void imprime(Cientista* dado, long int *linhas, char nome[50]);
+void libera(Cientista *dado);
 
 int contadorLinhas();
 
@@ -24,7 +25,7 @@ int main(int argc, char const *argv[])
 
     do
     {
-        printf("**** Menu ****\n 1 - Carregar Arquivos de Dados\n 2 - Emitir Relatório\n 3 - Sair\n Digite o item que deseja acessar: ");
+        printf("\n               Menu  \n 1 - Carregar Arquivos de Dados\n 2 - Emitir Relatório\n 3 - Sair\n\nDigite o item que deseja acessar: ");
         scanf("%d", &item);
 
         switch (item)
@@ -43,32 +44,48 @@ int main(int argc, char const *argv[])
 
             linhas = (long int *) malloc(quantidadeLinhas * sizeof(long int));
 
-            arqOriginal = fopen(nome, "r");
-            fseek(arqOriginal, 0, SEEK_SET);
-            fscanf(arqOriginal, "%s", primeiraLinha);
-            linhas[0] = ftell(arqOriginal);
+            if(arqOriginal = fopen(nome, "r")){
+                fseek(arqOriginal, 0, SEEK_SET);
+                fscanf(arqOriginal, "%s", primeiraLinha);
+                linhas[0] = ftell(arqOriginal);
 
-            // colunas do arquivo
+                // colunas do arquivo
 
-            int id;
-            int work_year, remote_ratio;
-            char experience_level[10], employment_type[10], job_title[100], salary_currency[10], employee_residence[10], company_location[10], company_size[10];
-            int salary, salary_in_usd;
-            while (fscanf(arqOriginal, "%d,%d,%[^,],%[^,],%[^,],%d,%[^,],%d,%[^,],%d,%[^,],%[^\n]", &id, &work_year, experience_level, employment_type, job_title, &salary, salary_currency, &salary_in_usd, employee_residence, &remote_ratio, company_location, company_size) == 12)
-            {
-                linhas[id+1] = ftell(arqOriginal);
-                instala(&arvore,salary_in_usd,id);
+                int id;
+                int work_year, remote_ratio;
+                char experience_level[10], employment_type[10], job_title[100], salary_currency[10], employee_residence[10], company_location[10], company_size[10];
+                int salary, salary_in_usd;
+                while (fscanf(arqOriginal, "%d,%d,%[^,],%[^,],%[^,],%d,%[^,],%d,%[^,],%d,%[^,],%[^\n]", &id, &work_year, experience_level, employment_type, job_title, &salary, salary_currency, &salary_in_usd, employee_residence, &remote_ratio, company_location, company_size) == 12)
+                {
+                    linhas[id+1] = ftell(arqOriginal);
+                    instala(&arvore,salary_in_usd,id);
+                }
+            }else{
+                printf("Erro ao abrir o arquivo");
             }
 
+            fclose(arqOriginal);
+
+            if(arvore!=NULL){
+                printf("Arvore criada com sucesso!\n");
+            }else{
+                printf("Erro ao criar arvore!\n");
+            }
             break;
 
         case 2:
-            printf("Ano de trabalho | Experiência | Tipo de emprego | Residência do empregado | Salário em USD | Título do trabalho   \n");
-            imprime(arvore, linhas, nome);
+            if(arvore!=NULL){
+                printf("\nAno de trabalho | Experiência | Tipo de emprego | Residência do empregado | Salário em USD | Título do trabalho   \n");
+                imprime(arvore, linhas, nome);
+            }else{
+                printf("\nArvore vazia!\n\n");
+            }
             break;
 
         case 3:
-            printf("Sair\n");
+            libera(arvore);
+            printf("Arvore liberada com sucesso!\n");
+            printf("Saindo...\n");
             break;
 
         default:
@@ -103,7 +120,7 @@ void instala(Cientista ** dado, int salario, int linha) {
 
 void imprime(Cientista *raiz, long int *linhas, char nome[50]) {
     FILE *arqOriginal;
-    arqOriginal = fopen(nome, "r+b");
+    arqOriginal = fopen(nome, "r");
     char linha[500];
     int id;
     int work_year, remote_ratio;
@@ -116,6 +133,7 @@ void imprime(Cientista *raiz, long int *linhas, char nome[50]) {
         printf("%d            | %s          | %s              | %s                      | %d | %s \n", work_year, experience_level, employment_type, employee_residence, salary_in_usd, job_title );
         imprime(raiz->direita, linhas, nome);
     }
+    fclose(arqOriginal);
 }
 
 int contadorLinhas(char nome[]) {
@@ -134,4 +152,16 @@ int contadorLinhas(char nome[]) {
         count++;
     }
     return count;
+}
+
+void libera(Cientista *dado){
+    if(dado){
+        libera(dado->esquerda);
+        libera(dado->direita);
+        /*(*dado)->esquerda = NULL;
+        (*dado)->direita = NULL;
+        (*dado)->salario = 0;
+        (*dado)->linha = 0;*/
+        free(dado);
+    }
 }
